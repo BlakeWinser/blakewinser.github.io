467665a1cd2484ec3840369c650b8743c1598886
Blake Winser <blakelwinser@gmail.com>
2021-06-07 04:20:30 +1000

    Updates

---
+++ precache-manifest.0cb969853ede2be6a8112234e501162c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4342ce6c3a4b5007a7cb2e22445233a",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "0d22d44cdc8a744eda6b",
+    "url": "/home/static/css/main.d11511b8.chunk.css"
+  },
+  {
+    "revision": "a827fdf11183b48f35fb",
+    "url": "/home/static/js/2.60a79c90.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/home/static/js/2.60a79c90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d22d44cdc8a744eda6b",
+    "url": "/home/static/js/main.01bcfb70.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "5cb954ba28f55c638ad703ff719db729",
+    "url": "/home/static/media/Blender_LowPolyArm.5cb954ba.PNG"
+  },
+  {
+    "revision": "64d8422609c464afbe406408e81f8a09",
+    "url": "/home/static/media/Blender_LowPolyCharacterModel.64d84226.PNG"
+  },
+  {
+    "revision": "44255d4012fbdfce556828dc9a7e0ca7",
+    "url": "/home/static/media/Blender_LowPolyGlock.44255d40.PNG"
+  },
+  {
+    "revision": "408ba85a98142ea0d4135114d73a9c16",
+    "url": "/home/static/media/Blender_TLOU_Rifle.408ba85a.png"
+  },
+  {
+    "revision": "e4ea11c0a5c809acb4f64fd1b165654b",
+    "url": "/home/static/media/Maya_BasicWoodenCrate.e4ea11c0.png"
+  },
+  {
+    "revision": "253734e0e82869bc2a90aa82ea011e9d",
+    "url": "/home/static/media/Mobile_LeapFroggy.253734e0.PNG"
+  },
+  {
+    "revision": "903a28f7d76b15f82e3bc54102302e78",
+    "url": "/home/static/media/Mobile_SpaceWar_Screenshot.903a28f7.png"
+  },
+  {
+    "revision": "1f64bb0a9baf185cb9e94af241f9139f",
+    "url": "/home/static/media/Unity_CurrentSideProject1.1f64bb0a.PNG"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
